1ff53228d4ef108b572e1125aa80643c07f6a8ce
manikalizroy <manikalizroy@gmail.com>
2026-03-01 00:15:31 +0530

    Revert "Updated login form"
    This reverts commit ca6a87f3047015c7e0810150f4bd56100707645f.

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    SECRET_KEY = "super-secret-key"
-    JWT_SECRET_KEY = "jwt-secret-key"
-
-    DB_HOST = "127.0.0.1"
-    DB_USER = "hostel_user"
-    DB_PASSWORD = "hostel123"
-    DB_NAME = "hostel_ease"
